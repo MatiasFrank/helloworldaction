76658f8ffd1ae0214e0befd98fde5ce000929fe7
Matias Frank Jensen <matias@nuntio.io>
2023-11-03 10:28:52 +0100

    Ping9

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 
 func root(w http.ResponseWriter, r *http.Request) {
 	log.Println("Got pinged")
-	w.Write([]byte("Ping8!"))
+	w.Write([]byte("Ping9!"))
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
